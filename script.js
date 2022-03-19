15e0172af778601cd2f3b3a908c00a778211abb8
aali <aaliyan1230@gmail.com>
2022-03-19 21:26:56 +0500

    inital commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const choicessss=["rock", "paper", "scissor"]
+let computerPlay=()=>{
+    let index = Math.floor(Math.random() * 3);
+    let item = choicessss[index];
+    
+    // Math.floor(Math.random()*options.length)
+    return item;
+}
+
+let gamePlay=(user, computer)=>{
+    let answer="";
+    user=user.toLowerCase();
+    computer=computer.toLowerCase();
+    if(user===computer){
+        answer='tied!';
+    }
+    else if(user=='rock'){
+        if(computer == "scissor"){
+            answer='user wins!';
+        }
+        else{
+            answer='computer wins!';
+        }
+    }
+    else if(user=='paper' ){
+        if(computer == 'rock'){
+            answer='user wins!';
+        }
+        else{
+            answer='computer wins!';
+        }
+    }
+    else if(user=='scissor'){
+        if(computer == "paper"){
+            answer='user wins!';
+        }
+        else{
+            answer='computer wins!';
+        }
+    }
+
+    return answer;
+}
+
+let game = ()=>{
+    for(let i=0;i<5;i++){
+        let userChoice = prompt("enter your choice","");
+        let result = gamePlay(userChoice,computerPlay());
+        console.log(result);
+    }
+}
+
+game();
